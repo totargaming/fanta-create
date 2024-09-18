de032fc07111d129a3d92ba63bb6333005d7836c
totargaming <totargaming@gmail.com>
2024-09-18 10:25:53 +0930

    Refactor image model to include additional properties and export the Image model

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,4 +1,4 @@
-import { Schema } from "mongoose";
+import { model, models, Schema } from "mongoose";
 
 const ImageSchema = new Schema({
     title: { type: String, required: true },
@@ -7,6 +7,16 @@ const ImageSchema = new Schema({
     secureUrl: { type: String, required: true },
     width: { type: Number },
     height: { type: Number },
-
+    config: { type: Object },
+    transformationUrl: { type: URL },
+    aspectRatio: { type: String },
+    color: { type: String },
+    prompt: { type: String },
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
 
 })
+const Image = models?.Image || model('Image', ImageSchema);
+
+export default Image;
