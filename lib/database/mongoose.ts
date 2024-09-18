ad5e14649f368cc665b27093e71eb0e9b96c7009
totargaming <totargaming@gmail.com>
2024-09-18 10:09:48 +0930

    Refactor package.json to add @mongodb/mongodb dependency

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,24 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+    conn: Mongoose | null;
+    promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+    cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+
+export const connectToDatabase = async () => {
+    if (cached.conn) return cached.conn;
+    if (!MONGODB_URL) throw new Error('Missing MONGODB_URL is not defined');
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, { dbName: 'FantaCreate', buferCommands: false });
+
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
