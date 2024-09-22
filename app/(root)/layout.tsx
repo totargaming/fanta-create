5047a714281874337d597c0c2732ec082f67141a
totargaming <totargaming@gmail.com>
2024-09-23 00:49:24 +0930

    Refactor layout component to fix sidebar spacing

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className="root">
-            < Sidebar />
+            <Sidebar />
             <MobileNav />
             <div className="root-container">
                 <div
