f632991436006a5106b1b172bbb86f70b9d48c9a
totargaming <totargaming@gmail.com>
2024-09-25 16:17:51 +0930

    Refactor MediaUploader component to use Cloudinary upload widget and add image preview

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -31,6 +31,7 @@ import {
 import { CustomField } from "./CustomField";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { updateCredits } from "@/lib/actions/user.actions";
+import MediaUploader from "./MediaUploader";
 export const formSchema = z.object({
   title: z.string(),
   aspectRatio: z.string().optional(),
@@ -130,7 +131,6 @@ const TransformationForm = ({
           className="w-full"
           render={({ field }) => <Input {...field} className="input-field" />}
         />
-
         {type === "fill" && (
           <CustomField
             control={form.control}
@@ -211,6 +211,15 @@ const TransformationForm = ({
             control={form.control}
             name="publicId"
             className="flex size-full flex-col"
+            render={({ field }) => (
+              <MediaUploader
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+              />
+            )}
           />
         </div>
         <div className="flex flex-col gap-4">
