440fbd181a7fa40d5a2e72df5f58b910483b7128
totargaming <totargaming@gmail.com>
2024-12-26 15:35:29 +1030

    Add image retrieval functionality; implement getAllImages for pagination and search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,15 @@
+import { Collection } from "@/components/shared/Collection";
 import { navLinks } from "@/constants";
+import { getAllImages } from "@/lib/actions/image.actions";
+import { Images } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Home = () => {
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || "";
+  const images = await getAllImages({ page, searchQuery });
   return (
     <>
       <section className="home">
@@ -24,6 +30,14 @@ const Home = () => {
             </Link>
           ))}
         </ul>
+      </section>
+      <section className="sm:mt-12">
+        <Collection
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
       </section>
     </>
   );
