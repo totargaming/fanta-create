98468138f7c785ea47abb87ce4031c8b1c33f6af
totargaming <totargaming@gmail.com>
2024-12-31 15:23:24 +1030

    Refactor Collection button class for improved styling; fix immediate invocation of useEffect in TransformationForm

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -164,7 +164,7 @@ const TransformationForm = ({
           [fieldName === "prompt" ? "prompt" : "to"]: value,
         },
       }));
-    }, 1000);
+    }, 1000)();
     return onChangeField(value);
   };
   // TODO: Implement the onTransformHandler function
